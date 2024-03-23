aa37281d6466850e3aa8e081aed3b9c10d7f4d11
Nathaniel Escaro <butterman423@gmail.com>
2024-03-23 19:33:32 -0400

    feat: Blocks to render JSON and images in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import blueprintLogo from './assets/blueprint.png'
+import memberPicture from './assets/member.png'
+import profileIcon from './assets/profile.png'
+import * as contentJSON from './assets/data/content.json'
 import './App.css'
 
 import { useState } from 'react'
@@ -8,7 +11,7 @@ import FormDisplay from './components/FormDisplay'
 
 function App() {
   const [formData, setFormData] = useState({ name: "", email: "", year: "" })
-
+  
   return (
     <>
       <div className='logo'>
@@ -21,6 +24,31 @@ function App() {
         <BlueForm setFormData={setFormData}/>
         <FormDisplay formData={formData}/>
       </div>
+
+      <div className="">
+        <div className="">
+          <img src={memberPicture} alt="Member Picture"/>
+        </div>
+
+        <div className="">
+          <div className="">
+            <img src={profileIcon} alt="Profile Headshot"/>
+          </div>
+          <div className="">{ contentJSON.anectdote.quote }</div>
+          <div className="">{ contentJSON.anectdote.author }</div>
+        </div>
+
+        <div className="">
+          <div className="">{ contentJSON.content.header }</div>
+          <div className="">{ contentJSON.content.body }</div>
+        </div>
+
+        <div className="">
+          <img src="" alt="Missing Asset"/>
+        </div>
+      </div>
+
+      <p>Blueprint is cool!</p>
     </>
   )
 }
