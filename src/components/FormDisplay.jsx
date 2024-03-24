c6ba9171a8fc78df9d451fde740425b61a02eee9
Nathaniel Escaro <butterman423@gmail.com>
2024-03-24 01:07:22 -0400

    feat: Styles for displaying form data
    Implemented most styling rules for the display
    section. Still unpolished.

--- src/components/FormDisplay.jsx
+++ src/components/FormDisplay.jsx
@@ -1,17 +1,17 @@
 export default function FormDisplay({ formData }) {
     return (
-        <div className=''>
-            <div className=''>
-                <p>Name</p>
-                <p>{ formData.name }</p>
+        <div className='form-display-container'>
+            <div className='form-display-item'>
+                <p className='form-display-header'>Name</p>
+                <p className='form-display-content'>{ formData.name }</p>
             </div>
-            <div className=''>
-                <p>Email</p>
-                <p>{ formData.email }</p>
+            <div className='form-display-item'>
+                <p className='form-display-header'>Email</p>
+                <p className='form-display-content'>{ formData.email }</p>
             </div>
-            <div className=''>
-                <p>Graduation Year</p>
-                <p>{ formData.year }</p>
+            <div className='form-display-item'>
+                <p className='form-display-header'>Graduation Year</p>
+                <p className='form-display-content'>{ formData.year }</p>
             </div>
         </div>
     )
